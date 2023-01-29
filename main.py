01e646b2b556ddaba9297bada9aef6c0c00f530a
Artem <Artyom08112006@gmail.com>
2023-01-29 21:50:14 +0300

    added gitignore and spaces between letters

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import pyautogui
 morze = {" ": "*", 'a': '•—', 'b': '—•••', 'c': '—•—•', 'd': '—••', 'e': '•', 'f': '••—•', 'g': '——•', 'h': '••••', 'i': '••', 'j': '•———', 'k': '—•—', 'l': '•—••', 'm': '——', 'n': '—•', 'o': '———', 'p': '•——•', 'q': '——•—', 'r': '•—•', 's': '•••', 't': '—', 'u': '••—', 'v': '•••—', 'w': '•——', 'x': '—••—', 'y': '—•——', 'z': '——••'}
 
 def translate(word):
-    code = ""
+    code = []
     word = word.lower()
     for letter in word:
-        code += morze.get(letter)
+        code.append(morze.get(letter))
     return code
 
 def dot():
@@ -22,12 +22,14 @@ def dash():
 
 def get_light_code(word):
     code = translate(word)
-    for i in code:
-        if i == "•":
-            dot()
-        elif i == "—":
-            dash()
-        elif i == "*":
-            time.sleep(2)
+    for sumbol in code:
+        for i in sumbol:
+            if i == "•":
+                dot()
+            elif i == "—":
+                dash()
+            elif i == "*":
+                time.sleep(2)
+        time.sleep(3)
 
 get_light_code(input("Введите слово для кодирования: "))
